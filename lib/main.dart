6e706a5be5cf88f25a3a3f54968b3fa9b15fe9c1
Sumit Thakur <51052011+sumitt1080@users.noreply.github.com>
2020-10-01 09:54:41 +0530

    Added Only Portrait mode
    I added this because it,s my thought that dating apps can best be viwed only in portrait mode. Have a look at it and decide.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
